4ec48a5f9bc115d3085bc7d94dd7dcdc17c97d44
yakuboff311216-netizen <yakuboff311216@gmail.com>
2026-06-23 06:56:10 +0300

    Add: 9) Foydalanuvchi chiqishni tanlashi [yakuboff311216-netizen]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/9__Foydalanuvchi_chiqishni_tanlashi.py
@@ -0,0 +1,13 @@
+yashirin_son = 3
+
+while True:
+    son = int(input())
+    
+    if son == 0:
+        print("Exit")
+        break
+    elif son == yashirin_son:
+        print("Correct")
+        break
+    else:
+        print("Wrong")
